4a5f0da215f204612fd702e618acf2f0fe1fe409
HarryW217 <harrywalker217@gmail.com>
2024-01-10 15:26:02 +0000

    Added event listener to the form to enable the written name to show up on the desired list

--- 16-naughty-list-nice-list/index.pack.js
+++ 16-naughty-list-nice-list/index.pack.js
@@ -38,6 +38,23 @@ function sort() {
   naughtyList.innerHTML = naughty.join("");
 }
 
+let nameInput = document.getElementById("name");
+const chosenList = document.getElementById("lists");
+const submitBtn = document.getElementById("submit");
+submitBtn.addEventListener("click", addToList);
+
+function addToList() {
+  event.preventDefault();
+  const newSortee = document.createElement("li");
+  newSortee.textContent = nameInput.value;
+  if (chosenList.value === "Nice") {
+    niceList.appendChild(newSortee);
+  } else if (chosenList.value === "Naughty") {
+    naughtyList.appendChild(newSortee);
+  }
+  nameInput.value = "";
+}
+
 /** Challenge: 
   - Write the JavaScript to sort the people in sorteesArr into the naughty and nice lists, according to whether they have been good or not. Then display the names in the relevant place in the DOM.
 **/
